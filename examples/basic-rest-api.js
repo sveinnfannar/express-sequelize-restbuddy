c1949c120eda0ed2519d6a0d841bac83dbee8f79
Sveinn Fannar Kristjansson <sveinnfannar@gmail.com>
2014-11-13 15:06:37 +0000

    Updated example

--- examples/basic-rest-api.js
+++ examples/basic-rest-api.js
@@ -10,8 +10,8 @@ var Promise = require('bluebird');
 var restBuddy = require('../');
 var Sequelize = require('sequelize');
 var express = require('express');
-var bodyParser = require('body-parser')
-var debug = require('debug')('examples/basic-rest-api');
+var bodyParser = require('body-parser');
+var debug = require('debug')('express-sequelize-restbuddy');
 var app = express();
 app.use(bodyParser.json());
 
@@ -29,7 +29,7 @@ var Channel = sequelize.define('Channel', {
 });
 
 var ScheduleItem = sequelize.define('ScheduleItem', {
-  title: Sequelize.STRING
+  from: Sequelize.DATE
 });
 
 Channel.hasMany(ScheduleItem);
@@ -41,6 +41,12 @@ sequelize.sync({ force: true })
       Channel.create({ name: 'My Channel 1', price: 5.99 }),
       Channel.create({ name: 'My Channel 2', price: 0.99 })
     ])
+  })
+  .spread(function (channel1, channel2) {
+    return ScheduleItem.create({ from: new Date() })
+      .then(function (scheduleItem) {
+        return channel1.addScheduleItem(scheduleItem);
+      })
   });
 
 /**
@@ -50,19 +56,13 @@ app.get('/channels', restBuddy(sequelize, {
   conditionTransformers: {
     search: function (value) { return { name: { like: '%' + value + '%' } } }
   }
-}));
+}), sendData);
 
-app.get('/channels/:id', restBuddy(sequelize));
-app.patch('/channels/:id', restBuddy(sequelize));
-app.put('/channels/:id', restBuddy(sequelize));
-
-//app.get('/channels', restBuddy(sequelize));
-app.get('/users/:id/channels', restBuddy(sequelize));
-
-app.get('/users/:id/channels/:id', function (req, res, next) {
-  console.log(typeof req.params.id);
-  res.json(req);
-});
+app.get('/channels/:id', restBuddy(sequelize), sendData);
+app.put('/channels/:id', restBuddy(sequelize), sendData);
+app.patch('/channels/:id', restBuddy(sequelize), sendData);
+app.get('/channels/:id/scheduleItems', restBuddy(sequelize), sendData);
+app.get('/channels/:id/scheduleItems/:id', restBuddy(sequelize), sendData);
 
 /**
  * Server
@@ -71,3 +71,12 @@ app.get('/users/:id/channels/:id', function (req, res, next) {
 var server = app.listen(3000, function() {
   console.log('Listening on port %d', server.address().port);
 });
+
+/**
+ * Utility Functions
+ */
+
+// The last middleware to be called in the response chain to write the data to the response
+function sendData (req, res) {
+  res.send(req.data);
+}
